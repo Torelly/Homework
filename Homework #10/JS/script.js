d523284b4118d3beedc835fb47b3eee68aec7dd7
Torelly <115055130+Torelly@users.noreply.github.com>
2022-11-04 13:53:16 +0400

    Add files via upload

---
+++ Homework #10/JS/script.js
@@ -0,0 +1,11 @@
+let anteba = () => {
+    document.getElementById("light").src="IMG/on.png";
+    document.getElementById("universal").innerHTML = "ჩაქრობა";
+    document.getElementById("universal").style.backgroundColor = "#880000"
+}
+
+let chakroba = () => {
+    document.getElementById("light").src="IMG/off.png";
+    document.getElementById("universal").innerHTML = "ანთება";
+    document.getElementById("universal").style.backgroundColor = "#008840"
+}
